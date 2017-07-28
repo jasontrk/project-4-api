2a7f4a0c45108ae3c20bb5bf84c0bab61e391c30
Mercedes Samavi <msamavi@mofo.com>
2017-07-28 14:36:09 +0100

    minimum styling

--- app/serializers/medium_serializer.rb
+++ app/serializers/medium_serializer.rb
@@ -3,13 +3,17 @@ class MediumSerializer < ActiveModel::Serializer
   has_many :dislikes
   has_many :faves
   belongs_to :creator
-  attributes :id, :name, :release_date, :first_air_date, :poster_path_src, :poster_path_big, :overview, :genre_ids, :tmdb_id, :like_ids, :dislike_ids, :fave_ids
+  attributes :id, :name, :release_date, :first_air_date, :poster_path_src, :poster_path_big, :poster_path_med, :overview, :genre_ids, :tmdb_id, :like_ids, :dislike_ids, :fave_ids
 
   def poster_path_src
     "https://image.tmdb.org/t/p/w45#{object.poster_path}"
   end
 
+  def poster_path_med
+    "https://image.tmdb.org/t/p/w92#{object.poster_path}"
+  end
+
   def poster_path_big
-    "https://image.tmdb.org/t/p/w500#{object.poster_path}"
+    "https://image.tmdb.org/t/p/w300#{object.poster_path}"
   end
 end
